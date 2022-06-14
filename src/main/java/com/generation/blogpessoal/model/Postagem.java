8e6a8728fcda7bbb2ac6f46ad2b4eda1e6151b72
Edgard Henrique <edigarhenriqu@gmail.com>
2022-06-14 15:31:04 -0300

    Swagger

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -39,7 +39,25 @@ public class Postagem {
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
 	
-	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
 	public long getId() {
 		return id;
--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -15,6 +15,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuario")
 public class Usuario {
@@ -27,7 +29,8 @@ public class Usuario {
 	@Size(min = 5, message = "O nome deve ter no mínimo 5 caracteres")
 	private String nome;
 	
-	@NotNull
+	@Schema(example = "email@email.com.br")
+	@NotNull(message = "O atributo Usuário é Obrigatório!")
 	@Email(message = "O atributo Usuário deve ser um email válido!")
 	private String usuario;
 	
